dceb1a0cafad1750062a30d9bdd844dae038aa1a
PhamThiKimDanh <126800584+PhamThiKimDanh1802@users.noreply.github.com>
2025-07-04 22:35:14 +0700

    5CN

---
+++ 1.py
@@ -0,0 +1,10 @@
+x=5
+print(x)
+
+# This program prints the value of x
+# x = 10    
+# print("Value of x is:", x)
+# This program prints the value of x
+# x = 20
+# print("Value of x is:", x)
+# This program prints the value of x
